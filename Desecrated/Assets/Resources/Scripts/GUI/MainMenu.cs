4a2eec1ec2d6f357bf751574538ef45252cb2c9a
code-11 <brendan.ritter@yahoo.com>
2017-12-13 00:03:21 -0500

    Started on very first steps in unity. Got gitignore working.

---
+++ Desecrated/Assets/Resources/Scripts/GUI/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+	public Button lvlOne;
+    void Start()
+    {
+        lvlOne.onClick.AddListener(onLvlOneClick);
+    }
+
+    void onLvlOneClick()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+}

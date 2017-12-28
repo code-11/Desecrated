bf4b82270f6da32bbf2d41861fe08f85f1b03588
code-11 <brendan.ritter@yahoo.com>
2017-12-27 23:51:55 -0500

    Added standard assets. Scaled project larger. NavPath.

--- Desecrated/Assets/Resources/Scripts/Movement/PlayerMovement.cs
+++ Desecrated/Assets/Resources/Scripts/Movement/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public float speed=100.0f;	
+	public float radius=.2f;
+	public bool debug=false;
 
 	// Update is called once per frame
 	void Update () {
@@ -17,11 +19,13 @@ public class PlayerMovement : MonoBehaviour {
 		movDelta.Normalize();
 		movDelta*=speed;
 		movDelta*=Time.deltaTime;
-		float radius=.1f;
 		Vector3 newPos=new Vector3(x+movDelta.x,y,z+movDelta.z);
-		Collider[] colliders=Physics.OverlapSphere(newPos,radius);
+		int buildingMask=(1<<LayerMask.NameToLayer("Buildings"));
+		Collider[] colliders=Physics.OverlapSphere(newPos,radius,buildingMask);
 		if (colliders.Length==0){
 			transform.Translate(movDelta);
+		}else{
+			Debug.Log(colliders[0].name);
 		}
 	}
 }

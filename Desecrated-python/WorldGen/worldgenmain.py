c6266637eff7dc76475643ffe24d5f6cee3f410a
code-11 <brendan.ritter@yahoo.com>
2019-02-21 19:24:22 -0500

    Got heading working

--- Desecrated-python/WorldGen/worldgenmain.py
+++ Desecrated-python/WorldGen/worldgenmain.py
@@ -48,6 +48,19 @@ def haversine(lon1, lat1, lon2, lat2):
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles
     return c * r
 
+def heading(lat1,lon1,lat2,lon2):
+	lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
+	X=math.cos(lat2)*math.sin(lon1-lon2)
+	Y=math.cos(lat1)*math.sin(lat2)-math.sin(lat1)*math.cos(lat2)*math.cos(lon1-lon2)
+	brng= math.atan2(X,Y)
+	brng=(brng+(math.pi*2))%(math.pi*2)
+	return (2*math.pi)-brng 
+
+def plot_great_circle(lat1,lon1,lat2,lon2):
+	plt.plot([lon1, lon2], [lat1, lat2],
+         color='blue', linewidth=1, marker='o',
+         transform=ccrs.Geodetic(),
+         )
 
 # plt.plot([ny_lon, delhi_lon], [ny_lat, delhi_lat],
 #          color='blue', linewidth=2, marker='o',
@@ -68,16 +81,22 @@ lon2=145
 lat3=48.8567
 lon3=2.3508
 
-print(to_geodetic(*to_geocentric(lat1,lon1)))
+# print(to_geodetic(*to_geocentric(lat1,lon1)))
 
-
-
-# fig = plt.figure()
-# ax = fig.add_subplot(1, 1, 1,
-#                      projection=ccrs.PlateCarree())
-# ax.coastlines()
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1,
+                     projection=ccrs.PlateCarree())
+ax.coastlines()
 # ax.scatter(lons,lats)
 # ax.plot(lons, lats,transform=ccrs.Geodetic())
-# ax.set_global()
+ax.set_global()
+print(heading(0,0,1,1))
+
+print(heading(1,1,30,30))
+
+print(heading(30,30,1,1))
+
+print(heading(-30,-30,10,10))
+
 
 # plt.show()

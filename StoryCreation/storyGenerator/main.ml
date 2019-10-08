a65ee9b8aa7628ec51a6a2bf0072f6f299b0c2d0
code-11 <brendan.ritter@yahoo.com>
2019-10-07 21:46:25 -0400

    Added some ocaml structure
    Add

---
+++ StoryCreation/storyGenerator/main.ml
@@ -0,0 +1,27 @@
+open Printf;;
+
+type concept = Death 
+			| Fire  
+			| Decay 
+			| Restoration 
+			| Knowledge 
+			| Magic 
+			| Holy 
+			| Life 
+			| Wealth;;
+
+type action = string * (concept list);;
+
+type ur_action = string * (action list) * (concept list);;
+
+type item = string * (ur_action list) * (concept list);;
+
+let average a b =
+  (a +. b) /. 2.0;;
+let (melted:action) = ("Melted",[Fire]);;
+let (destroy:ur_action) = ("Destroy",[melted],[Death]);;
+let (forged:action) = ("Forged",[Life]);;
+let (create:ur_action) = ("Create",[forged],[Life]);;
+let (necklace:item) = ("Necklace",[create; destroy],[Wealth]);;
+
+Printf.printf "%.3f" (average 5.0 10.0)
